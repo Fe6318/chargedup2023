6165c93eeef78d41e8cce4cd9b6425e2dedf0da9
unknown <mvandehey04@icloud.com>
2023-02-21 19:52:43 -0600

    slow arm down update

--- src/main/java/frc/robot/commands/ManualArm.java
+++ src/main/java/frc/robot/commands/ManualArm.java
@@ -9,11 +9,14 @@ import frc.robot.subsystems.Arm;
 public class ManualArm extends CommandBase {
   private Arm arm;
   private Joystick operator;
+  private Timer timer;
+  private boolean hasMoved = false;
 
   public ManualArm(Arm arm, Joystick operator) {
     this.arm = arm;
     addRequirements(arm);
     this.operator = operator;
+    timer = new Timer();
   }
 
   // Called when the command is initially scheduled.
@@ -26,12 +29,35 @@ public class ManualArm extends CommandBase {
   @Override
   public void execute(){
     double x = operator.getRawAxis(Constants.armUp);
-    arm.Move(x);
+    // if trigger pressed
+    if(x < .1){
+      // check if trigger moved
+      if(hasMoved){
+        timer.reset();
+        timer.start();
+        hasMoved = false;
+      }
+      // slows arm if timer is less than 2 seconds
+      if(timer.get() < 2){
+        arm.Move(.1);
+      }
+      // otherwise timer and arm stop
+      else{
+        timer.stop();
+        timer.reset();
+        arm.Move(0);
+      }
+    }
+    // otherwise arm moves like normal
+    else{
+      arm.Move(x*.5);
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted){
+    
   }
 
   // Returns true when the command should end.
